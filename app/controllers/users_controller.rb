be35c62f14343555b86b3d1a1892c249ab18e964
Ramin Mammadzada <remki-design@Hotmail.com>
2020-09-23 01:34:14 +0300

    Update routes and controller for editin user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    redirect_to users_path(@user)
+  end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
